ea5ed842f86e2f10a6f54811a8dffb137e647242
Rutwij-Patil <patil.rutwij@gmail.com>
2026-04-14 05:00:54 +0530

    Initial Commit

---
+++ Meridian/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Meridian
+//
+//  Created by Rutwij on 14/04/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
